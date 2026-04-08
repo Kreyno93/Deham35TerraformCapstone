6bd7faa899c971e903c602980a860865b5f550cd
Kreyno93 <michael.wagener@neuefische.de>
2026-04-08 17:24:20 +0200

    adding private rtb and associate the private subnet with the private rtb

--- main.tf
+++ main.tf
@@ -47,3 +47,15 @@ resource "aws_route_table_association" "public-tf-wordpress-rtb-association" {
   subnet_id      = aws_subnet.public-tf-wordpress-subnet.id
   route_table_id = aws_route_table.public-tf-wordpress-rtb.id
 }
+resource "aws_route_table" "private-tf-wordpress-rtb" {
+  vpc_id = aws_vpc.wordpress-tf-vpc.id
+
+  tags = {
+    Name = "private-tf-wordpress-rtb"
+  }
+}
+
+resource "aws_route_table_association" "private-tf-wordpress-rtb-association" {
+  subnet_id      = aws_subnet.private-tf-wordpress-subnet.id
+  route_table_id = aws_route_table.private-tf-wordpress-rtb.id
+}
