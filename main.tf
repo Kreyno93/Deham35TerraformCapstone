cf1fcb2aaf5a9e2053b771538f3155f185fa1799
Kreyno93 <michael.wagener@neuefische.de>
2026-04-09 12:52:21 +0200

    removing unncessary comments

--- main.tf
+++ main.tf
@@ -1,13 +1,1 @@
-# The file is currently empty
-
-# data "aws_ami" "amazon_linux_2023" {
-#   most_recent = true
-#   owners      = ["137112412989"]
-
-#   filter {
-#     name   = "name"
-#     values = ["al2023-ami-2023.*-x86_64"]
-#   }
-# }
-
 
